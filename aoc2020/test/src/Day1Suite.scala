3a6505447269456a2f54e8890e316c7fd44c7140
Jason Dove <jason@jasondove.me>
2020-12-02 06:09:18 -0600

    add tests for solutions

--- aoc2020/test/src/Day1Suite.scala
+++ aoc2020/test/src/Day1Suite.scala
@@ -11,11 +11,28 @@ object Day1Suite extends SimpleIOSuite {
       _ <- expect(result.map(_.product) == Some(514579)).failFast
     } yield success
   }
+
+  simpleTest("part 1 solution") {
+    for {
+      input <- Day1.readInput("day1.txt")
+      result <- IO(Day1.search(input, 2, 2020))
+      _ <- expect(result.map(_.product) == Some(935419)).failFast
+    } yield success
+  }
+
   simpleTest("part 2 example") {
     for {
       input <- Day1.readInput("day1-example.txt")
       result <- IO(Day1.search(input, 3, 2020))
       _ <- expect(result.map(_.product) == Some(241861950)).failFast
+    } yield success
+  }
+
+  simpleTest("part 2 solution") {
+    for {
+      input <- Day1.readInput("day1.txt")
+      result <- IO(Day1.search(input, 3, 2020))
+      _ <- expect(result.map(_.product) == Some(49880012)).failFast
     } yield success
   }
 }
--- aoc2020/test/src/Day2Suite.scala
+++ aoc2020/test/src/Day2Suite.scala
@@ -11,11 +11,28 @@ object Day2Suite extends SimpleIOSuite {
       _ <- expect(result == 2).failFast
     } yield success
   }
+
+  simpleTest("part 1 solution") {
+    for {
+      input <- Day2.readInputOne("day2.txt")
+      result <- IO(Day2.search(input))
+      _ <- expect(result == 477).failFast
+    } yield success
+  }
+
   simpleTest("part 2 example") {
     for {
       input <- Day2.readInputTwo("day2-example.txt")
       result <- IO(Day2.search(input))
       _ <- expect(result == 1).failFast
+    } yield success
+  }
+
+  simpleTest("part 2 solution") {
+    for {
+      input <- Day2.readInputTwo("day2.txt")
+      result <- IO(Day2.search(input))
+      _ <- expect(result == 686).failFast
     } yield success
   }
 }
