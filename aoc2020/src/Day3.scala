fc8757c281f20324bd56ba8ab66d74b5cb915a66
Jason Dove <jason@jasondove.me>
2020-12-03 04:30:31 -0600

    add day 3

---
+++ aoc2020/src/Day3.scala
@@ -0,0 +1,70 @@
+package aoc2020
+
+import cats.effect._
+
+sealed abstract class MapSquare extends Product with Serializable
+
+object MapSquare {
+  final case object OpenSquare extends MapSquare
+  final case object Tree extends MapSquare
+}
+
+case class MapRow(squares: List[MapSquare])
+case class Map(rows: List[MapRow])
+
+object Map {
+  def from(lines: List[String]): Map = {
+    new Map(lines.map(row(_)))
+  }
+
+  def row(line: String): MapRow = {
+    new MapRow(line.map(square(_)).toList)
+  }
+
+  def square(char: Char): MapSquare =
+    char match {
+      case '#' => MapSquare.Tree
+      case _   => MapSquare.OpenSquare
+    }
+}
+
+object Day3 extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] = {
+    for {
+      input <- readInput("day3.txt")
+      result <- IO(search(input, 3, 1))
+      _ <- printResult(result)
+      result <- IO(multiSearch(input))
+      _ <- printResult(result)
+    } yield ExitCode.Success
+  }
+
+  def readInput(resourceName: String): IO[Map] =
+    IO(Map.from(os.read(os.resource / resourceName).split("\n").toList))
+
+  def search(map: Map, slopeRight: Int, slopeDown: Int): Long = {
+    val height = map.rows.length
+    val width = map.rows(0).squares.length
+    @annotation.tailrec
+    def inner(acc: Long, x: Int, y: Int): Long = {
+      if (y >= height) acc
+      else if (map.rows(y).squares(x) == MapSquare.Tree)
+        inner(acc + 1, (x + slopeRight) % width, y + slopeDown)
+      else
+        inner(acc, (x + slopeRight) % width, y + slopeDown)
+    }
+    inner(0, 0, 0)
+  }
+
+  def multiSearch(map: Map): Long = {
+    val one = search(map, 1, 1)
+    val two = search(map, 3, 1)
+    val three = search(map, 5, 1)
+    val four = search(map, 7, 1)
+    val five = search(map, 1, 2)
+    one * two * three * four * five
+  }
+
+  def printResult(result: Long): IO[Unit] =
+    IO(println(s"found $result trees"))
+}
---
+++ aoc2020/test/src/Day3Suite.scala
@@ -0,0 +1,38 @@
+package aoc2020
+
+import weaver._
+import cats.effect._
+
+object Day3Suite extends SimpleIOSuite {
+  simpleTest("part 1 example") {
+    for {
+      input <- Day3.readInput("day3-example.txt")
+      result <- IO(Day3.search(input, 3, 1))
+      _ <- expect(result == 7).failFast
+    } yield success
+  }
+
+  simpleTest("part 1 solution") {
+    for {
+      input <- Day3.readInput("day3.txt")
+      result <- IO(Day3.search(input, 3, 1))
+      _ <- expect(result == 156).failFast
+    } yield success
+  }
+
+  simpleTest("part 2 example") {
+    for {
+      input <- Day3.readInput("day3-example.txt")
+      result <- IO(Day3.multiSearch(input))
+      _ <- expect(result == 336).failFast
+    } yield success
+  }
+
+  simpleTest("part 2 solution") {
+    for {
+      input <- Day3.readInput("day3.txt")
+      result <- IO(Day3.multiSearch(input))
+      _ <- expect(result == 3521829480L).failFast
+    } yield success
+  }
+}
