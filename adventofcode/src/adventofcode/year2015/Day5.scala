755b9f69cb34018d5280029b19557dc740c937b6
Jason Dove <jason@jasondove.me>
2020-12-05 12:25:38 -0600

    add 2015 day 5

---
+++ adventofcode/src/adventofcode/year2015/Day5.scala
@@ -0,0 +1,66 @@
+package adventofcode.year2015
+
+import adventofcode.Day
+
+case class Day5Context(classifier: String => Boolean)
+
+object Day5 extends Day[List[String], Day5Context](2015, 5) {
+  override def transformInput(lines: List[String]): List[String] = lines
+
+  override def partOneContext(): Option[Day5Context] =
+    Some(Day5Context(isNicePartOne))
+
+  override def partTwoContext(): Option[Day5Context] =
+    Some(Day5Context(isNicePartTwo))
+
+  override def process(input: List[String], context: Option[Day5Context]): Option[Long] =
+    context.map { ctx =>
+      input.filter(ctx.classifier).length.toLong
+    }
+
+  private def isNicePartOne(str: String): Boolean = {
+    val vowels = Set('a', 'e', 'i', 'o', 'u')
+    val badStrings = Set("ab", "cd", "pq", "xy")
+
+    def threeVowels(str: String): Boolean = str.count(vowels.contains) >= 3
+
+    def doubleLetter(str: String): Boolean = {
+      @annotation.tailrec
+      def loop(str: String): Boolean = {
+        if (str.tail.isEmpty) false
+        else if (str.head == str.tail.head) true
+        else loop(str.tail)
+      }
+      loop(str)
+    }
+
+    def noBadStrings(str: String): Boolean =
+      badStrings.foldLeft(true)(_ && !str.contains(_))
+
+    threeVowels(str) && doubleLetter(str) && noBadStrings(str)
+  }
+
+  private def isNicePartTwo(str: String): Boolean = {
+    def notOverlappingPair(str: String): Boolean = {
+      @annotation.tailrec
+      def loop(str: String): Boolean = {
+        if (str.length < 4) false
+        else if (str.substring(2).contains(str.take(2))) true
+        else loop(str.tail)
+      }
+      loop(str)
+    }
+
+    def repeatsWithSpace(str: String): Boolean = {
+      @annotation.tailrec
+      def loop(str: String): Boolean = {
+        if (str.length < 3) false
+        else if (str.head == str.charAt(2)) true
+        else loop(str.tail)
+      }
+      loop(str)
+    }
+
+    notOverlappingPair(str) && repeatsWithSpace(str)
+  }
+}
