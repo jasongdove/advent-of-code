dc470a80e9208fd67199dcfef06452e1df515c27
Jason Dove <jason@jasondove.me>
2020-12-08 17:43:25 -0600

    add 2015 day 23

---
+++ adventofcode/src/adventofcode/year2015/Day23.scala
@@ -0,0 +1,80 @@
+package adventofcode.year2015
+
+import adventofcode.Day
+
+sealed trait AsmOperation
+
+object AsmOperation {
+  case object Hlf extends AsmOperation
+  case object Tpl extends AsmOperation
+  case object Inc extends AsmOperation
+  case object Jmp extends AsmOperation
+  case object Jie extends AsmOperation
+  case object Jio extends AsmOperation
+}
+
+case class AsmInstruction(operation: AsmOperation, register: Option[Char], operand: Option[Int])
+
+object AsmInstruction {
+  val halfPattern = "hlf ([ab])".r
+  val triplePattern = "tpl ([ab])".r
+  val incrementPattern = "inc ([ab])".r
+  val jumpPattern = "jmp ([\\+-]?\\d+)".r
+  val jumpEvenPattern = "jie ([ab]), ([\\+-]?\\d+)".r
+  val jumpOnePattern = "jio ([ab]), ([\\+-]?\\d+)".r
+
+  def from(line: String): AsmInstruction =
+    line match {
+      case halfPattern(register)      => AsmInstruction(AsmOperation.Hlf, Some(register.head), None)
+      case triplePattern(register)    => AsmInstruction(AsmOperation.Tpl, Some(register.head), None)
+      case incrementPattern(register) => AsmInstruction(AsmOperation.Inc, Some(register.head), None)
+      case jumpPattern(offset)        => AsmInstruction(AsmOperation.Jmp, None, Some(offset.toInt))
+      case jumpEvenPattern(register, offset) =>
+        AsmInstruction(AsmOperation.Jie, Some(register.head), Some(offset.toInt))
+      case jumpOnePattern(register, offset) =>
+        AsmInstruction(AsmOperation.Jio, Some(register.head), Some(offset.toInt))
+    }
+}
+
+case class Day23Context(startingRegisters: Map[Char, Int])
+
+object Day23 extends Day[List[AsmInstruction], Day23Context, Int](2015, 23) {
+
+  override def transformInput(lines: List[String]): List[AsmInstruction] =
+    lines.map(AsmInstruction.from)
+
+  override def partOneContext(): Option[Day23Context] =
+    Some(Day23Context(Map.empty))
+
+  override def partTwoContext(): Option[Day23Context] =
+    Some(Day23Context(Map('a' -> 1)))
+
+  override def process(input: List[AsmInstruction], context: Option[Day23Context]): Option[Int] =
+    context.flatMap { ctx =>
+      evaluate(input, ctx.startingRegisters).get('b')
+    }
+
+  def evaluate(instructions: List[AsmInstruction], startingRegisters: Map[Char, Int]): Map[Char, Int] = {
+    @annotation.tailrec
+    def eval(acc: Map[Char, Int], currentIndex: Int): Map[Char, Int] =
+      instructions.lift(currentIndex) match {
+        case Some(AsmInstruction(AsmOperation.Hlf, Some(register), None)) =>
+          eval(acc.updated(register, acc.getOrElse(register, 0) / 2), currentIndex + 1)
+        case Some(AsmInstruction(AsmOperation.Tpl, Some(register), None)) =>
+          eval(acc.updated(register, acc.getOrElse(register, 0) * 3), currentIndex + 1)
+        case Some(AsmInstruction(AsmOperation.Inc, Some(register), None)) =>
+          eval(acc.updated(register, acc.getOrElse(register, 0) + 1), currentIndex + 1)
+        case Some(AsmInstruction(AsmOperation.Jmp, None, Some(offset))) =>
+          eval(acc, currentIndex + offset)
+        case Some(AsmInstruction(AsmOperation.Jie, Some(register), Some(offset))) =>
+          val o = if (acc.getOrElse(register, 0) % 2 == 0) offset else 1
+          eval(acc, currentIndex + o)
+        case Some(AsmInstruction(AsmOperation.Jio, Some(register), Some(offset))) =>
+          val o = if (acc.getOrElse(register, 0) == 1) offset else 1
+          eval(acc, currentIndex + o)
+        case _ => acc
+      }
+
+    eval(startingRegisters, 0)
+  }
+}
