c36bd3f155370f44ef265b60a927ea3ebf7d04d8
Jason Dove <jason@jasondove.me>
2022-12-04 09:28:16 -0600

    add 2022 day 4

---
+++ year2022/Day4.scala
@@ -0,0 +1,38 @@
+package adventofcode.year2022
+
+import adventofcode.Day
+import cats.effect._
+
+object Day1 extends IOApp {
+
+  case class AssignmentPair(one: Range, two: Range) {
+    def fullyContains: Boolean = (two.contains(one.head) && two.contains(one.last)) || (one.contains(two.head) && one.contains(two.last))
+    def intersects: Boolean = !two.intersect(one).isEmpty
+  }
+
+  case class Context(solve: List[AssignmentPair] => Int)
+
+  object Runner extends Day[List[AssignmentPair], Context, Int](2022, 4) {
+    override def transformInput(lines: List[String]): List[AssignmentPair] =
+      lines.map { line =>
+        var split = line.split(',')
+        var oneSplit = split(0).split('-')
+        var twoSplit = split(1).split('-')
+        var one = oneSplit.head.toInt.to(oneSplit.last.toInt)
+        var two = twoSplit.head.toInt.to(twoSplit.last.toInt)
+        AssignmentPair(one, two)
+      }.toList
+
+    override def partOneContext(): Option[Context] =
+      Some(Context(_.count(_.fullyContains)))
+
+    override def partTwoContext(): Option[Context] =
+      Some(Context(_.count(_.intersects)))
+
+    override def process(input: List[AssignmentPair], context: Option[Context]): Option[Int] = {
+      context.map(_.solve(input))
+    }
+  }
+
+  override def run(args: List[String]): IO[ExitCode] = Runner.run(args)
+}
