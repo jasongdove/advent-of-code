115c16a802871f21a0176eca5412c6da5c10db07
Jason Dove <jason@jasondove.me>
2022-12-16 11:47:31 -0600

    wip, doesn't work

--- year2022/Day16.scala
+++ year2022/Day16.scala
@@ -1,26 +1,105 @@
 package adventofcode.year2022
 
-import adventofcode.Day
+import adventofcode.{Day, Edge}
 import cats.effect._
+import adventofcode.WeightedGraph
+import scala.collection.mutable.PriorityQueue
 
 object Day16 extends IOApp {
-  case class Input(whatever: List[Int])
+  case class Valve(id: String, flowRate: Int, targets: Set[String])
 
-  case object Input {
-    def from(lines: List[String]): Input = {
-      Input(List.empty)
+  case object Valve {
+    def from(line: String): Valve = {
+      val pattern = "Valve (.*) has flow rate=(\\d+); tunnels? leads? to valves? (.*)".r
+      val pattern(from, weight, to) = line
+      val targets = to.split(", ").toSet
+      Valve(from, weight.toInt, targets)
     }
   }
 
-  case class Context(solve: Input => Long)
+  def solve(valves: Map[String, Valve], start: String): Long = {
+    case class Position(minutesRemaining: Int, totalPressure: Int, openValves: List[String], valveId: String)
 
-  object Runner extends Day[Input, Context, Long](2022, 16) {
-    override def transformInput(lines: List[String]): Input =
-      Input.from(lines)
+    println(valves.mkString("\n"))
+
+    def potentialPressure(totalPressure: Int, minutesRemaining: Int, openValves: List[String]) =
+      totalPressure + valves.filterNot(openValves.contains).map(v => v._2.flowRate * minutesRemaining).sum
+
+    def priorityOrder(p: Position) = p.totalPressure
+
+    var max = 0;
+
+    val queue = scala.collection.mutable
+      .PriorityQueue[Position](Position(30, 0, List.empty, start))(Ordering.by(priorityOrder))
+
+    var count = 0;
+    while (queue.nonEmpty && count < 10_00_000_000) {
+      if (count % 1_000_000 == 0) {
+        println(s"count: ${count}, max: ${max}")
+      }
+      count = count + 1
+      val Position(minutesRemaining, totalPressure, openValves, currentValveId) = queue.dequeue()
+      if (totalPressure > max) {
+        max = totalPressure
+      }
+      if (minutesRemaining == 0) {
+        // do nothing
+        // if potential pressure <= max, continue
+      } else if (potentialPressure(totalPressure, minutesRemaining, openValves) > max) {
+        // println(s"Position(${minutesRemaining} min, ${totalPressure}, ${openValves}, ${currentValveId}) max ${max}")
+        val valve = valves(currentValveId)
+
+        val next = if (openValves.contains(currentValveId)) {
+          valve.targets.map(target =>
+            Position(
+              minutesRemaining - 1,
+              totalPressure,
+              openValves,
+              target
+            )
+          )
+        } else {
+          valve.targets.flatMap(target =>
+            Seq(
+              Position(
+                minutesRemaining - 1,
+                totalPressure,
+                openValves,
+                target
+              ),
+              Position(
+                math.max(minutesRemaining - 2, 0),
+                totalPressure + (minutesRemaining - 1) * valve.flowRate,
+                openValves :+ valve.id,
+                target
+              )
+            )
+          )
+        }
+
+        // println(s"${next.mkString("\n")}")
+
+        queue.enqueue(next.toSeq: _*)
+      }
+    }
+    // }
+
+    // max
+    0
+  }
+
+  case class Context(solve: List[Valve] => Long)
+
+  object Runner extends Day[List[Valve], Context, Long](2022, 16) {
+    override def transformInput(lines: List[String]): List[Valve] =
+      lines.map(Valve.from)
 
     override def partOneContext(): Option[Context] =
       Some(Context(input => {
         val _ = input
+        // println(input.mkString("\n"))
+        val valves = input.map(v => (v.id, v)).toMap
+        println(solve(valves, input.head.id))
         0
       }))
 
@@ -30,7 +109,7 @@ object Day16 extends IOApp {
         0
       }))
 
-    override def process(input: Input, context: Option[Context]): Option[Long] =
+    override def process(input: List[Valve], context: Option[Context]): Option[Long] =
       context.map(_.solve(input))
   }
 
