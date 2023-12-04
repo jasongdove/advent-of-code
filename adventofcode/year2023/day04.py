c9b0ac5e612bed958b0ea886530b102cde8b19d4
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2023-12-03 22:24:03 -0600

    add skeleton

---
+++ adventofcode/year2023/day04.py
@@ -0,0 +1,14 @@
+from adventofcode import Day
+
+
+class Day04(Day):
+    def __init__(self):
+        super().__init__(2023, 4)
+
+    def part01(self):
+        text = super()._part01_input()
+        return 0
+
+    def part02(self):
+        text = super()._part01_input()
+        return 0
