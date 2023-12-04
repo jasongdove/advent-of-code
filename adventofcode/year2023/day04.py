ebed0f0d849a95beb8518acaf34383ac4a434017
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2023-12-04 04:00:01 -0600

    part 1 works

--- adventofcode/year2023/day04.py
+++ adventofcode/year2023/day04.py
@@ -1,4 +1,20 @@
 from adventofcode import Day
+import re
+
+
+class Card:
+
+    def __init__(self, line):
+        res = re.match('Card\s+(\d+):(.+)\|(.+)', line)
+        self.card_number = res.group(1)
+        self.winning_numbers = set(map(int, res.group(2).split()))
+        self.all_numbers = set(map(int, res.group(3).split()))
+
+    def score(self):
+        count = len(self.all_numbers.intersection(self.winning_numbers))
+        if count == 0:
+            return 0
+        return 2 ** (count - 1)
 
 
 class Day04(Day):
@@ -7,7 +23,8 @@ class Day04(Day):
 
     def part01(self):
         text = super()._part01_input()
-        return 0
+        cards = list(map(Card, text.splitlines()))
+        return sum(map(lambda c: c.score(), cards))
 
     def part02(self):
         text = super()._part01_input()
