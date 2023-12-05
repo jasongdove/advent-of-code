e060324b31136e51487580a88e253a48b5c9ec87
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2023-12-05 06:21:26 -0600

    part 2 works

--- adventofcode/year2023/day05.py
+++ adventofcode/year2023/day05.py
@@ -1,124 +1,75 @@
 from adventofcode import Day
-
-class Map:
-
-    def __init__(self, dest_start, source_start, length):
-        self.dest_start = dest_start
-        self.source_start = source_start
-        self.length = length
-
-    def map(self, value):
-        if value in range(self.source_start, self.source_start + self.length):
-            return self.dest_start + (value - self.source_start)
-        return None
-
-
-class MapSet:
-
-    def __init__(self):
-        self.maps = []
-
-    def add(self, m):
-        self.maps.append(m)
-
-    def map(self, value):
-        for m in self.maps:
-            result = m.map(value)
-            if result is not None:
-                return result
-        return value
-
-
+import re
+import math
 
 class Day05(Day):
     def __init__(self):
         super().__init__(2023, 5)
+        self.reg = re.compile("(\\d+)")
+
+    def process(self, lines, value):
+        next_value = value
+        for i in range(1, len(lines)):
+            line = lines[i]
+            if ':' in line:
+                continue
+            elif line == '':
+                value = next_value
+                continue
+            elif value is not None:
+                range_map = list(map(int, line.split(' ')))
+                if range_map[1] <= value < (range_map[1] + range_map[2]):
+                    next_value = value - range_map[1] + range_map[0]
+                    value = None
+        return value or next_value
 
     def part01(self):
         text = super()._part01_input()
-        seeds = []
-        seed_to_soil = MapSet()
-        soil_to_fertilizer = MapSet()
-        fertilizer_to_water = MapSet()
-        water_to_light = MapSet()
-        light_to_temp = MapSet()
-        temp_to_humidity = MapSet()
-        humidity_to_location = MapSet()
         lines = text.splitlines()
-        for i in range(0, len(lines)):
+        result = list(map(int, self.reg.findall(lines[0])))
+        next_result = []
+        for i in range(1, len(lines)):
             line = lines[i]
-            if line.startswith("seeds:"):
-                seeds.extend(map(int, filter(lambda x: x.isdigit(), line.split(':')[1].split(' '))))
-            elif line.startswith("seed-to-soil map"):
-                while True:
-                    i += 1
-                    line = lines[i]
-                    if line == "":
-                        break
-                    values = list(map(int, line.split(' ')))
-                    seed_to_soil.add(Map(values[0], values[1], values[2]))
-            elif line.startswith("soil-to-fertilizer map"):
-                while True:
-                    i += 1
-                    line = lines[i]
-                    if line == "":
-                        break
-                    values = list(map(int, line.split(' ')))
-                    soil_to_fertilizer.add(Map(values[0], values[1], values[2]))
-            elif line.startswith("fertilizer-to-water map"):
-                while True:
-                    i += 1
-                    line = lines[i]
-                    if line == "":
-                        break
-                    values = list(map(int, line.split(' ')))
-                    fertilizer_to_water.add(Map(values[0], values[1], values[2]))
-            elif line.startswith("water-to-light map"):
-                while True:
-                    i += 1
-                    line = lines[i]
-                    if line == "":
-                        break
-                    values = list(map(int, line.split(' ')))
-                    water_to_light.add(Map(values[0], values[1], values[2]))
-            elif line.startswith("light-to-temperature map"):
-                while True:
-                    i += 1
-                    line = lines[i]
-                    if line == "":
-                        break
-                    values = list(map(int, line.split(' ')))
-                    light_to_temp.add(Map(values[0], values[1], values[2]))
-            elif line.startswith("temperature-to-humidity map"):
-                while True:
-                    i += 1
-                    line = lines[i]
-                    if line == "":
-                        break
-                    values = list(map(int, line.split(' ')))
-                    temp_to_humidity.add(Map(values[0], values[1], values[2]))
-            elif line.startswith("humidity-to-location map"):
-                while True and i < len(lines) - 1:
-                    i += 1
-                    line = lines[i]
-                    if line == "":
-                        break
-                    values = list(map(int, line.split(' ')))
-                    humidity_to_location.add(Map(values[0], values[1], values[2]))
-        print(seeds)
-        min_loc = 10000000000000
-        for seed in seeds:
-            soil = seed_to_soil.map(seed)
-            fert = soil_to_fertilizer.map(soil)
-            water = fertilizer_to_water.map(fert)
-            light = water_to_light.map(water)
-            temp = light_to_temp.map(light)
-            humidity = temp_to_humidity.map(temp)
-            loc = humidity_to_location.map(humidity)
-            min_loc = min(min_loc, loc)
-            print(f'{seed} => {soil} => {fert} => {water} => {light} => {temp} => {humidity} => {loc}')
-        return min_loc
+            if ':' in line:
+                next_result = []
+                continue
+            elif line == '':
+                next_result.extend(result)
+                result = next_result
+                continue
+            range_map = list(map(int, line.split(' ')))
+            for j in result[:]:
+                if range_map[1] <= j <= (range_map[1] + range_map[2]):
+                    value = j - range_map[1] + range_map[0]
+                    next_result.append(value)
+                    result.remove(j)
+        next_result.extend(result)
+        result = next_result
+        return min(result)
 
     def part02(self):
         text = super()._part01_input()
-        return 0
+        lines = text.splitlines()
+        result = list(map(int, self.reg.findall(lines[0])))
+        best_seed = None
+        seed_step = None
+        min_result = 10000000000000000000
+        # for each seed range
+        for r in range(0, len(result), 2):
+            lo = result[r]
+            hi = lo + result[r + 1] - 1
+            # check a handful of seeds using large steps, to find the best seed range
+            step = max(1, int(math.ceil(hi - lo) / 10_000.0))
+            for t in range(lo, hi + 1, step):
+                res = self.process(lines, t)
+                if res < min_result:
+                    min_result = res
+                    best_seed = t
+                    seed_step = step
+
+        # check every number around that specific seed (+/- step)
+        min_result = 10000000000000000000
+        for i in range(best_seed - seed_step, best_seed + seed_step + 1):
+            res = self.process(lines, i)
+            min_result = min(min_result, res)
+        return min_result
