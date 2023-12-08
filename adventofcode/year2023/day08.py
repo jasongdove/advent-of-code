73e7289578f3609d6aa63bb5083e595d07628051
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2023-12-07 23:08:26 -0600

    part 1

--- adventofcode/year2023/day08.py
+++ adventofcode/year2023/day08.py
@@ -1,4 +1,17 @@
 from adventofcode import Day
+import re
+import collections
+import itertools
+
+class Instr:
+
+    def __init__(self, line):
+        reg = re.compile("([A-Z]{3})\\s+=\\s+\\(([A-Z]{3}),\\s+([A-Z]{3})\\)")
+        m = reg.match(line)
+        self.key = m.groups(0)[0]
+        self.left = m.groups(0)[1]
+        self.right = m.groups(0)[2]
+        print(f'{self.key} => {self.left}, {self.right}')
 
 
 class Day08(Day):
@@ -7,7 +20,25 @@ class Day08(Day):
 
     def part01(self):
         text = super()._part01_input()
-        return 0
+        lines = text.splitlines()
+        directions = [c for c in lines[0]]
+        rest = lines[2:]
+        x = map(Instr, rest)
+        m = {}
+        for i in x:
+            m[i.key] = i
+        pos = "AAA"
+        count = 0
+        for c in itertools.cycle(directions):
+            current = m[pos]
+            if current.key == "ZZZ":
+                break
+            if c == "R":
+                pos = current.right
+            else:
+                pos = current.left
+            count += 1
+        return count
 
     def part02(self):
         text = super()._part01_input()
