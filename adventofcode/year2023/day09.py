aafcf67e143fb36f8f84a4d28c1210ab5033cb73
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2023-12-09 04:18:57 -0600

    part 2

--- adventofcode/year2023/day09.py
+++ adventofcode/year2023/day09.py
@@ -2,16 +2,6 @@ from adventofcode import Day
 import re
 
 
-class Instr:
-
-    def __init__(self, line):
-        reg = re.compile("([A-Z0-9]{3})\\s+=\\s+\\(([A-Z0-9]{3}),\\s+([A-Z0-9]{3})\\)")
-        m = reg.match(line)
-        self.key = m.groups(0)[0]
-        self.left = m.groups(0)[1]
-        self.right = m.groups(0)[2]
-
-
 class Day09(Day):
     def __init__(self):
         super().__init__(2023, 9)
@@ -47,12 +37,11 @@ class Day09(Day):
                 next_line.append(last_line[i] - last_line[i - 1])
             history.append(next_line)
         reverse = list(reversed(history))
-        reverse[0].append(0)
+        reverse[0].insert(0, 0)
         for i in range(1, len(reverse)):
-            next_item = (reverse[i - 1][-1] + reverse[i][-1])
-            reverse[i].append(next_item)
-        print(reverse)
-        return reverse[-1][-1]
+            next_item = (reverse[i][0] - reverse[i - 1][0])
+            reverse[i].insert(0, next_item)
+        return reverse[-1][0]
 
     def part01(self):
         text = super()._part01_input()
