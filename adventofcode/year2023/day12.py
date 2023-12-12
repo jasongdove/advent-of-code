097d7de55bd1675193d77422aeb699f130199703
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2023-12-12 02:31:37 -0600

    part 1 (slow)

--- adventofcode/year2023/day12.py
+++ adventofcode/year2023/day12.py
@@ -1,13 +1,43 @@
 from adventofcode import Day
+from collections import deque
+import re
 
 
 class Day12(Day):
     def __init__(self):
         super().__init__(2023, 12)
+        self.reg = re.compile('(#+)')
+        self.bin_map = {'0': '.', '1': '#'}
+
+    def test_line(self, line: str, cont: list[int]) -> bool:
+        sizes = self.reg.finditer(line)
+        groups = [len(y) for y in [x.groups()[0] for x in sizes]]
+        return groups == cont
+
+    def solve_line(self, line: str) -> int:
+        s = line.split()
+        records = s[0]
+        cont = list(map(int, s[1].split(',')))
+        #print(records)
+        #print(cont)
+        q_count = records.count('?')
+        #print(q_count)
+        maybe_lines = []
+        for i in range(0, 2**q_count):
+            maybe_line = []
+            binary = deque([self.bin_map[x] for x in bin(i)[2:].zfill(q_count)])
+            for j in range(0, len(records)):
+                if records[j] == '?':
+                    maybe_line.append(binary.popleft())
+                else:
+                    maybe_line.append(records[j])
+            maybe_lines.append("".join(maybe_line))
+        return sum(1 if self.test_line(x, cont) else 0 for x in maybe_lines)
 
     def part01(self):
         text = super()._part01_input()
-        return 0
+        arrangements = [self.solve_line(x) for x in text.splitlines()]
+        return sum(arrangements)
 
     def part02(self):
         text = super()._part01_input()
