b48b62152fc8a61192c2a113a09b0d7017382bf1
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2023-12-15 21:39:04 -0600

    skeleton

---
+++ adventofcode/year2023/day16.py
@@ -0,0 +1,14 @@
+from adventofcode import Day
+
+
+class Day16(Day):
+    def __init__(self):
+        super().__init__(2023, 16)
+
+    def part01(self):
+        text = super()._part01_input()
+        return 0
+
+    def part02(self):
+        text = super()._part01_input()
+        return 0
