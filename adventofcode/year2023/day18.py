87a697d61c9ac47628351010f9d294325e863215
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2023-12-17 20:45:43 -0600

    skeleton

---
+++ adventofcode/year2023/day18.py
@@ -0,0 +1,19 @@
+from adventofcode import Day
+
+
+class Day18(Day):
+    def __init__(self):
+        super().__init__(2023, 18)
+
+    def part01(self):
+        text = super()._part01_input()
+        the_map = [[c for c in line] for line in text.splitlines()]
+        return 0
+
+    def part02(self):
+        text = super()._part01_input()
+        the_map = [[c for c in line] for line in text.splitlines()]
+        return 0
+
+
+
