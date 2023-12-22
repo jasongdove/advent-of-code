4d45c3d82b2aca1b2cd0d8ba21295abc26881a7f
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2023-12-21 18:31:08 -0600

    add 2023 day 21 (#23)
    * add remaining skeletons
    
    * both parts

---
+++ tests/2023/test_day21.py
@@ -0,0 +1,16 @@
+import unittest
+from adventofcode.year2023 import Day21
+
+
+class TestDay21(unittest.TestCase):
+
+    def setUp(self):
+        self.day = Day21()
+
+    def test_part01(self):
+        p1 = self.day.part01()
+        self.assertEqual(p1, 3671)
+
+    def test_part02(self):
+        p2 = self.day.part02()
+        self.assertEqual(p2, 609708004316870)
