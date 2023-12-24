5fbfcfb27caceb0e6cbc1cd82c8594749a42cf52
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2023-12-24 05:43:47 -0600

    part 2

--- adventofcode/year2023/day24.py
+++ adventofcode/year2023/day24.py
@@ -1,5 +1,6 @@
 import re
 from itertools import combinations
+import z3
 
 from shapely import Point, Polygon
 from shapely.geometry import LineString, box
@@ -36,6 +37,25 @@ class Day24(Day):
             extended_line = LineString(points_sorted_by_distance[:2])
         return extended_line
 
+    @staticmethod
+    def do_lines_intersect(combo, lo, hi) -> bool:
+        line1 = combo[0][1]
+        line2 = combo[1][1]
+        intersect: Point = line1.intersection(line2)
+        if not intersect.is_empty and lo <= intersect.x <= hi and lo <= intersect.y <= hi:
+            ax1 = combo[0][0][0]
+            ax2 = ax1 + combo[0][0][3]
+            bx1 = combo[1][0][0]
+            bx2 = bx1 + combo[1][0][3]
+            dax = intersect.x - ax1
+            dax2 = intersect.x - ax2
+            dbx = intersect.x - bx1
+            dbx2 = intersect.x - bx2
+            if abs(dax2) > abs(dax) or abs(dbx2) > abs(dbx):
+                return False
+            return True
+        return False
+
     def part01(self):
         lo = 200000000000000
         hi = 400000000000000
@@ -51,23 +71,26 @@ class Day24(Day):
         extended_lines = list(map(lambda li: (li, self.extend_line(polygon, li)), lines))
         combo: combinations[tuple[tuple[float, float, float, float, float, float], tuple[LineString, LineString]]]
         for combo in combinations(extended_lines, 2):
-            line1 = combo[0][1]
-            line2 = combo[1][1]
-            intersect: Point = line1.intersection(line2)
-            if not intersect.is_empty and lo <= intersect.x <= hi and lo <= intersect.y <= hi:
-                ax1 = combo[0][0][0]
-                ax2 = ax1 + combo[0][0][3]
-                bx1 = combo[1][0][0]
-                bx2 = bx1 + combo[1][0][3]
-                dax = intersect.x - ax1
-                dax2 = intersect.x - ax2
-                dbx = intersect.x - bx1
-                dbx2 = intersect.x - bx2
-                if abs(dax2) > abs(dax) or abs(dbx2) > abs(dbx):
-                    continue
+            if self.do_lines_intersect(combo, lo, hi):
                 total += 1
         return total
 
     def part02(self):
         text = super()._part01_input()
-        return 0
+        lines = []
+        for text_line in text.splitlines():
+            match = self.reg.match(text_line)
+            line = (float(match.group(1)), float(match.group(2)), float(match.group(3)),
+                    float(match.group(4)), float(match.group(5)), float(match.group(6)))
+            lines.append(line)
+
+        sx, sy, sz, dx, dy, dz = z3.Reals('sx sy sz dx dy dz')
+        solver = z3.Solver()
+        for i, line in enumerate(lines[:3]):
+            ti = z3.Real(f't{i}')
+            solver.add(ti > 0)
+            solver.add(sx + ti * dx == line[0] + ti * line[3])
+            solver.add(sy + ti * dy == line[1] + ti * line[4])
+            solver.add(sz + ti * dz == line[2] + ti * line[5])
+        solver.check()
+        return sum(solver.model()[v].as_long() for v in [sx, sy, sz])
